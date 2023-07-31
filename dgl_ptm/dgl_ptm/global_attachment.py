0ec63ebac7718667ec7021abe8148acf9a6a6c1b
cpranav93 <p.chandramouli@esciencecenter.nl>
2023-07-31 14:44:47 +0200

    Added global attachment functionality

--- dgl_ptm/dgl_ptm/global_attachment.py
+++ dgl_ptm/dgl_ptm/global_attachment.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
+import dgl
+from dgl import AddEdge, AddReverse
 
-# global_attachment - randomly connects different agents
+def global_attachment(agent_graph, ratio: float):
+    '''
+        global_attachment - randomly connects different agents globally based on a ratio
 
-def global_attachment():
-    print ('Code goes here')
+        Args: 
+            agent_graph: DGLGraph with agent nodes and edges connecting agents
+            ratio: ratio of number of new edges to add to total existing edges in graph
+
+        Output:
+            Modified agent_graph with new edges introduced 
+    '''
+    
+    # Add edges based on ratio
+    agent_graph = AddEdge(ratio=ratio)(agent_graph)
+
+    # Add reverse edges
+    agent_graph = AddReverse()(agent_graph)
+
+    # Remove duplicate edges
+    agent_graph = dgl.to_simple(agent_graph, return_counts='cnt')
