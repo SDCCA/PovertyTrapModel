27effe3bd65aee69330622183e80b8d52d677da6
Pranav Chandramouli <cpranav93@yahoo.com>
2023-09-05 11:58:44 +0200

    Revert "Agent update"

--- dgl_ptm/dgl_ptm/agent/agent_update.py
+++
@@ -1,7 +0,0 @@
-# agent_update - Updates the state of the agent based on income generation and money trades
-
-def agent_update(model_graph):
-    model_graph.ndata['k'] = model_graph.ndata['k'] + model_graph.ndata['net_trade']
-    income_generation(model_graph, method = 'pseudo_income_generation')
-    wealth_consumption(model_graph, method='pseudo_consumption')
-    model_graph.ndata['k'] = model_graph.ndata['k'] + model_graph.ndata['income'] - model_graph.ndata['wealth_consumption']
