f3dc6f9631d96513820c3c8ac7d1963443ebaa16
cpranav93 <p.chandramouli@esciencecenter.nl>
2023-11-30 12:09:49 +0100

    removed pseudo models

--- dgl_ptm/dgl_ptm/agent/wealth_consumption.py
+++ dgl_ptm/dgl_ptm/agent/wealth_consumption.py
@@ -6,9 +6,7 @@ from scipy.optimize import minimize_scalar
 
 def wealth_consumption(model_graph, model_params=None, method='pseudo_consumption'):
     # Calculate wealth consumed
-    if method == 'pseudo_consumption':
-        _pseudo_wealth_consumption(model_graph)
-    elif method == 'fitted_consumption':
+    if method == 'fitted_consumption':
         _fitted_wealth_consumption(model_graph)
     elif method == 'bellman_consumption':
         _bellman_wealth_consumption(model_graph,model_params)
@@ -16,9 +14,6 @@ def wealth_consumption(model_graph, model_params=None, method='pseudo_consumptio
         raise NotImplementedError("Incorrect method received. \
                          Method needs to be 'pseudo_consumption','fitted_consumption', or 'bellman_consumption'")
     
-
-def _pseudo_wealth_consumption(model_graph):
-    model_graph.ndata['wealth_consumption'] = model_graph.ndata['wealth']*1./3.
     
 def _fitted_wealth_consumption(model_graph):
     model_graph.ndata['wealth_consumption'] = 0.64036047*torch.log(model_graph.ndata['wealth'])
